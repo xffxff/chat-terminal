75efb915c7857f5b86f260d194d4158c2f930792
xffxff <1247714429@qq.com>
2023-06-07 08:09:05 +0800

    add some comments

--- src/openai.rs
+++ src/openai.rs
@@ -71,7 +71,40 @@ pub(crate) async fn chat_completions(
             let item = item.unwrap();
             let item_str = std::str::from_utf8(&item).unwrap();
             for chunk in item_str.split('\n').filter(|chunk| chunk.starts_with("data:")) {
+                // To be able to deserialize the item_str, we need to split it into chunks
+                // In general, the chunks look like this:
+                // data: {
+                //     "choices": [
+                //         {
+                //         "delta": {
+                //             "content": "\n\n"
+                //         },
+                //         "finish_reason": null,
+                //         "index": 0
+                //         }
+                //     ],
+                //     "created": 1677825464,
+                //     "id": "chatcmpl-6ptKyqKOGXZT6iQnqiXAH8adNLUzD",
+                //     "model": "gpt-3.5-turbo-0301",
+                //     "object": "chat.completion.chunk"
+                // }
+                // data: {
+                //     "choices": [
+                //         {
+                //         "delta": {
+                //             "content": "2"
+                //         },
+                //         "finish_reason": null,
+                //         "index": 0
+                //         }
+                //     ],
+                //     "created": 1677825464,
+                //     "id": "chatcmpl-6ptKyqKOGXZT6iQnqiXAH8adNLUzD",
+                //     "model": "gpt-3.5-turbo-0301",
+                //     "object": "chat.completion.chunk"
+                // }
                 let chunk = chunk.trim_start_matches("data: ");
+                // The last chunk is "data: [DONE]", which means the stream is done
                 if chunk.starts_with("[DONE]") {
                     return;
                 }
